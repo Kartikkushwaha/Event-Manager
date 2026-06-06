949e7debaabb7892377a619275ac7da08b1b3172
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-06-06 07:58:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpzCghQIIGbPkySYWTPNXvlcsnzsWoBQM",
+  authDomain: "eventease-c0bd9.firebaseapp.com",
+  projectId: "eventease-c0bd9",
+  storageBucket: "eventease-c0bd9.firebasestorage.app",
+  messagingSenderId: "720737113769",
+  appId: "1:720737113769:web:3a7fb2f8a4750448347bb8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+
+
+//for main page slide animation
 const slides = document.querySelectorAll(".slide");
 
 let current = 0;
@@ -17,3 +42,63 @@ setInterval(() => {
     slides[current].classList.add("active");
 
 }, 3000);
+
+
+document.getElementById("loginBtn")
+.addEventListener("click", async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+        const result =
+        await signInWithPopup(auth, provider);
+
+        const user = result.user;
+
+        alert(
+          `Welcome ${user.displayName}`
+        );
+
+        console.log(user);
+
+    } catch(error) {
+
+        console.error(error);
+
+        alert("Login Failed");
+
+    }
+
+});
+
+
+document.getElementById("signupBtn")
+.addEventListener("click", async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+        const result =
+        await signInWithPopup(auth, provider);
+
+        const user = result.user;
+
+        alert(
+          `Account Created: ${user.displayName}`
+        );
+
+        console.log(user);
+
+    } catch(error) {
+
+        console.error(error);
+
+        alert("Signup Failed");
+
+    }
+
+});
+
+
