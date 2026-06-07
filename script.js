7b6ad2994ca33964088c53124f6fe71302074919
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-06-07 20:01:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -84,3 +84,43 @@ document.getElementById("googleBtn")
 
 });
 
+
+// dashboard script
+
+const name =
+localStorage.getItem("userName");
+
+const email =
+localStorage.getItem("userEmail");
+
+const photo =
+localStorage.getItem("userPhoto");
+
+document.getElementById("name")
+.textContent = name;
+
+document.getElementById("email")
+.textContent = email;
+
+if(photo){
+
+    const img =
+    document.getElementById(
+        "profilePhoto"
+    );
+
+    img.src = photo;
+
+    img.style.display = "block";
+
+}
+
+else{
+
+    document.getElementById(
+        "profileLetter"
+    ).textContent =
+    name.charAt(0).toUpperCase();
+
+}
+
