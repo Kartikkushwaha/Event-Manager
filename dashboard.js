6798aa21943f34b8e1a9e4176358bca2d95040b5
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-06-09 09:13:51 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -71,18 +71,63 @@ document
 
 });
 
-// dashboard modals and clicks
-// document
-// .getElementById("createEventBtn")
-// .addEventListener("click",()=>{
 
-//     document
-//     .getElementById("eventModal")
-//     .style.display="block";
+const themeBtn =
+document.getElementById(
+    "themeToggle"
+);
 
-// });
-// eventData.style="Traditional";
-// eventData.style="Cultural";
+const savedTheme =
+localStorage.getItem(
+    "theme"
+);
+
+if(savedTheme==="dark"){
+
+    document.body.classList.add(
+        "dark-mode"
+    );
+
+    themeBtn.textContent="☀️";
+
+}
+
+themeBtn.addEventListener(
+    "click",
+    ()=>{
+
+        document.body.classList.toggle(
+            "dark-mode"
+        );
+
+        if(
+            document.body.classList.contains(
+                "dark-mode"
+            )
+        ){
+
+            localStorage.setItem(
+                "theme",
+                "dark"
+            );
+
+            themeBtn.textContent="☀️";
+
+        }
+
+        else{
+
+            localStorage.setItem(
+                "theme",
+                "light"
+            );
+
+            themeBtn.textContent="🌙";
+
+        }
+
+    }
+);
 
 
 
