9f1c8341bf5923b631685d8a8402bcad7ffaf0a7
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-06-10 14:13:59 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -129,5 +129,69 @@ themeBtn.addEventListener(
     }
 );
 
+// redirection to my event
+
+const saveBtn =
+document.getElementById(
+    "saveBtn"
+);
+
+saveBtn.addEventListener(
+    "click",
+    ()=>{
+
+        const eventData={
+
+            eventName:
+            document.getElementById(
+                "name"
+            ).value,
+
+            eventType:
+            document.getElementById(
+                "eventType"
+            ).value,
+
+            state:
+            document.getElementById(
+                "state"
+            ).value,
+
+            category:
+            document.getElementById(
+                "eventCategory"
+            ).value,
+
+            guestCount:
+            document.getElementById(
+                "guestCount"
+            ).value
+
+        };
+
+        let events=
+        JSON.parse(
+            localStorage.getItem(
+                "events"
+            )
+        ) || [];
+
+        events.push(eventData);
+
+        localStorage.setItem(
+            "events",
+            JSON.stringify(events)
+        );
+
+        alert(
+            "Event Saved Successfully!"
+        );
+
+        window.location.href =
+        "../my_events.html";
+
+    }
+);
+
 
 
