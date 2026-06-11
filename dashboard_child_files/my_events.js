da2ac106908addee87d1767a51ceb7b9bae80620
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-06-11 17:28:22 +0530

    Update my_events.js

--- dashboard_child_files/my_events.js
+++ dashboard_child_files/my_events.js
@@ -1,34 +1,72 @@
+import { initializeApp }
+from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+    getAuth,
+    onAuthStateChanged
+}
+from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    getFirestore,
+    collection,
+    getDocs
+}
+from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpzCghQIIGbPkySYWTPNXvlcsnzsWoBQM",
+  authDomain: "eventease-c0bd9.firebaseapp.com",
+  projectId: "eventease-c0bd9",
+  storageBucket: "eventease-c0bd9.firebasestorage.app",
+  messagingSenderId: "720737113769",
+  appId: "1:720737113769:web:3a7fb2f8a4750448347bb8"
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+const auth =
+getAuth(app);
+
+const db =
+getFirestore(app);
+
+
 const container =
 document.getElementById(
     "eventsContainer"
 );
 
-let events =
-JSON.parse(
-    localStorage.getItem(
-        "events"
-    )
-) || [];
+onAuthStateChanged(
+    auth,
+    async(user)=>{
 
-let workingEvents =
-[...events];
+        if(!user){
+            return;
+        }
 
-function renderEvents(){
+        const snapshot =
+        await getDocs(
 
-    container.innerHTML = "";
+            collection(
+                db,
+                "users",
+                user.uid,
+                "events"
+            )
 
-    if(
-        workingEvents.length===0
-    ){
+        );
 
-        container.innerHTML =
-        "<h2>No Events Found</h2>";
+        container.innerHTML="";
 
-        return;
-    }
+        snapshot.forEach(doc=>{
 
-    workingEvents.forEach(
-        (event,index)=>{
+            const event =
+            doc.data();
 
             const card =
             document.createElement(
@@ -38,7 +76,8 @@ function renderEvents(){
             card.className =
             "event-card";
 
-            card.innerHTML = `
+            card.innerHTML=`
+
                 <h2>
                 ${event.eventName}
                 </h2>
@@ -63,60 +102,13 @@ function renderEvents(){
                 ${event.guestCount}
                 </p>
 
-                <button
-                class="deleteBtn">
-                    Delete Event
-                </button>
             `;
 
-            const deleteBtn =
-            card.querySelector(
-                ".deleteBtn"
+            container.appendChild(
+                card
             );
 
-            deleteBtn
-            .addEventListener(
-                "click",
-                ()=>{
-
-                    workingEvents.splice(
-                        index,
-                        1
-                    );
-
-                    renderEvents();
-
-                }
-            );
-
-            container
-            .appendChild(card);
-
-        }
-    );
-
-}
-
-renderEvents();
-
-document
-.getElementById(
-    "updateBtn"
-)
-.addEventListener(
-    "click",
-    ()=>{
-
-        localStorage.setItem(
-            "events",
-            JSON.stringify(
-                workingEvents
-            )
-        );
-
-        alert(
-            "Changes Updated!"
-        );
+        });
 
     }
 );
