1ef237008e2d088ac9aff1473244646b1f902f2d
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-06-15 15:20:14 +0530

    Update my_events.js

--- dashboard_child_files/my_events.js
+++ dashboard_child_files/my_events.js
@@ -22,6 +22,48 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const themeBtn =
+document.getElementById(
+    "themeToggle"
+);
+const savedTheme =
+localStorage.getItem(
+    "theme"
+);
+if(savedTheme==="dark"){
+    document.body.classList.add(
+        "dark-mode"
+    );
+    themeBtn.textContent="☀️";
+}
+themeBtn.addEventListener(
+    "click",
+    ()=>{
+        document.body.classList.toggle(
+            "dark-mode"
+        );
+        if(
+            document.body.classList.contains(
+                "dark-mode"
+            )
+        ){
+            localStorage.setItem(
+                "theme",
+                "dark"
+            );
+            themeBtn.textContent="☀️";
+        }
+        else{
+            localStorage.setItem(
+                "theme",
+                "light"
+            );
+            themeBtn.textContent="🌙";
+        }
+    }
+);
+
+
 const container =
 document.getElementById(
     "eventsContainer"
