450b1d5cacf8c261787ac2db1be8938834fe84fe
Kartik Kumar <106160418+Kartikkushwaha@users.noreply.github.com>
2026-09-18 11:03:46 +0530

    Change Firebase config to new project settings
    Updated Firebase configuration with new project details.

--- frontend/dashboard_child_files/dash_dashboard.js
+++ frontend/dashboard_child_files/dash_dashboard.js
@@ -3,12 +3,13 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDpzCghQIIGbPkySYWTPNXvlcsnzsWoBQM",
-    authDomain: "eventease-c0bd9.firebaseapp.com",
-    projectId: "eventease-c0bd9",
-    storageBucket: "eventease-c0bd9.firebasestorage.app",
-    messagingSenderId: "720737113769",
-    appId: "1:720737113769:web:3a7fb2f8a4750448347bb8"
+    apiKey: "AIzaSyAhfKUyOjzZijEblzFhbm8f1budNPThudQ",
+    authDomain: "veloevent.firebaseapp.com",
+    projectId: "veloevent",
+    storageBucket: "veloevent.firebasestorage.app",
+    messagingSenderId: "185975138221",
+    appId: "1:185975138221:web:762d3d825a5972e4752324",
+    measurementId: "G-XVGT5GNNNZ"
 };
 
 const app = initializeApp(firebaseConfig);
